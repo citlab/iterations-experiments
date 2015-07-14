0479c60fd7a398eb9a2622675ad1b4b4c4fbb313
Lauritz Thamsen (on wally) <lauritz.thamsen@gmail.com>
2015-07-14 20:15:29 +0200

    Experiments: take the time of runs

--- experiments/kmeans/run.sh
+++ experiments/kmeans/run.sh
@@ -5,4 +5,4 @@ cd /home/lauritz/flink/
 HDFS_KMEANS_POINTS=/kmeans/points
 HDFS_KMEANS_CENTERS=/kmeans/centers
 
-./bin/flink run ./examples/flink-java-examples-0.9.0-KMeans.jar hdfs://$HDFS_KMEANS_POINTS hdfs://$HDFS_KMEANS_CENTERS hdfs:///kmeans/output_of_run_$RANDOM 5
+time bin/flink run ./examples/flink-java-examples-0.9.0-KMeans.jar hdfs://$HDFS_KMEANS_POINTS hdfs://$HDFS_KMEANS_CENTERS hdfs:///kmeans/output_of_run_$RANDOM 5

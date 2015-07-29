52390caba8975cf7fd1f98185791204fbbc2d55d
Lauritz Thamsen (on wally) <lauritz.thamsen@gmail.com>
2015-07-29 15:47:26 +0200

    K-means: Data generator script with skew and correct generator JAR

--- experiments/kmeans/data-gen.sh
+++ experiments/kmeans/data-gen.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #Paths
-GENERATOR_JAR="/home/lauritz/repos/adaptive-iterations/experiments/kmeans/flink-java-examples-0.9-SNAPSHOT-KMeans.jar"
+JARS="/home/lauritz/flink/lib/flink-dist-0.9.0.jar:/home/lauritz/flink/examples/flink-java-examples-0.9.0-KMeans.jar"
 HADOOP_PATH="/home/lauritz/hadoop-2.7.0"
 
 #Kmean variables
-points=40000000
-centers=5
+points=400000000
+centers=3
+skew="1,1,1"
 
 #Kmean generate into /tmp/ folder 
 echo "Generate kmeans data"
-java -cp $GENERATOR_JAR org.apache.flink.examples.java.clustering.util.KMeansDataGenerator $points $centers
+java -cp $JARS org.apache.flink.examples.java.clustering.util.KMeansDataGenerator -points $points -k $centers -skew $skew
 
 $HADOOP_PATH/bin/hdfs dfs -mkdir /kmeans
 

3fc1eac82818298a20f0106caf7c863196d1e261
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2015-09-18 15:04:41 +0200

    Also deploy custom flink builds to wally

--- deploy-custom-flink-to-peel.sh
+++ deploy-custom-flink-to-peel.sh
@@ -6,11 +6,26 @@
 # systems folder, e.g. $BUNDLE_BIN/adaptive-iterations/systems if not otherwise set
 # in the host's application.conf
 
-if [ -z "$PEEL_BIN_SYSTEM" ]; then
-    echo "variable PEEL_BIN_SYSTEMS has not been set"
+# note that both the systems folders need to exist (i.e. rsync:push peel bundle first,
+# )
+
+cd adaptive-flink
+mvn clean install -DskipTests
+
+mv build-target flink-0.9.0
+
+if [ -z "$LOCAL_PEEL_SYSTEMS" ]; then
+    echo "variable LOCAL_PEEL_SYSTEMS has not been set"
 else
-    cd adaptive-flink
-    mvn clean install -DskipTests
-    rm -r $PEEL_BIN_SYSTEMS/flink-0.9.0
-    mv build-target/ $PEEL_BIN_SYSTEMS/flink-0.9.0
+    rm -r $LOCAL_PEEL_SYSTEMS/flink-0.9.0
+    cp -R flink-0.9.0 $LOCAL_PEEL_SYSTEMS/flink-0.9.0
 fi
+
+if [ -z "$WALLY_PEEL_SYSTEMS" ]; then
+    echo "variable WALLY_PEEL_SYSTEMS has not been set"
+    echo "should include full path [user@]wally-master:/PATH/TO/PEEL/BUNDLE/SYSTEMS"
+else
+    scp -r flink-0.9.0 $WALLY_PEEL_SYSTEMS
+fi
+
+mv flink-0.9.0 build-target

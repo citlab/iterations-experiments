d46ae0d065f4035ca11b438adf4e9cbfebbac45b
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2015-09-22 12:11:11 +0200

    Deployment script: don't warn on first deployment

--- deploy-custom-flink-to-peel.sh
+++ deploy-custom-flink-to-peel.sh
@@ -16,14 +16,16 @@ mv build-target flink-0.9.0
 
 if [ -z "$LOCAL_PEEL_SYSTEMS" ]; then
     echo "variable LOCAL_PEEL_SYSTEMS has not been set"
+    echo "should be /PATH/TO/LOCAL/PEEL/BUNDLE/SYSTEMS"
+    echo "e.g. /home/[user]/bin/peel/adaptive-iterations/system"
 else
-    rm -r $LOCAL_PEEL_SYSTEMS/flink-0.9.0
+    rm -rf $LOCAL_PEEL_SYSTEMS/flink-0.9.0
     cp -R flink-0.9.0 $LOCAL_PEEL_SYSTEMS/flink-0.9.0
 fi
 
 if [ -z "$WALLY_PEEL_SYSTEMS" ]; then
     echo "variable WALLY_PEEL_SYSTEMS has not been set"
-    echo "should include full path [user@]wally-master:/PATH/TO/PEEL/BUNDLE/SYSTEMS"
+    echo "should be e.g. [user@]wally-master.cit.tu-berlin.de:/home/[user]/experiments/adaptive-iterations/systems"
 else
     scp -r flink-0.9.0 $WALLY_PEEL_SYSTEMS
 fi

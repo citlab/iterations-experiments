75d45791bb814ed6776f32f9a4760f9ae91bdf64
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2016-03-24 17:05:57 +0100

    Use Flink 1.0.0 for experiments

--- deploy-custom-flink-to-peel.sh
+++ deploy-custom-flink-to-peel.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-# build and deploy adaptive-flink as flink-0.9.0 to the bundle-bin
+# build and deploy custom-flink as flink-1.0.0 to the bundle-bin
 
 # assumes $PEEL_BIN_SYSTEMS exists and points to the $BUNDLE_BIN/adaptive-iterations's 
 # systems folder, e.g. $BUNDLE_BIN/adaptive-iterations/systems if not otherwise set
 # in the host's application.conf
 
-# note that both the systems folders need to exist (i.e. rsync:push peel bundle first,
-# )
+# note that both the systems folders need to exist (i.e. rsync:push bundle first)p
 
-cd adaptive-flink
+FLINK_VERSION="1.0.0"
+FLINK_FOLDER="flink-$FLINK_VERSION"
+
+cd custom-flink
 mvn clean install -DskipTests
 
-mv build-target flink-0.9.0
+mv build-target $FLINK_FOLDER
 
+# copy build to local peel systems
 if [ -z "$LOCAL_PEEL_SYSTEMS" ]; then
     echo "!! variable LOCAL_PEEL_SYSTEMS has not been set"
     echo "(should be /PATH/TO/LOCAL/PEEL/BUNDLE/SYSTEMS)"
     echo "e.g. /home/[user]/bin/peel/adaptive-iterations/system"
 else
-    echo ">> Copying flink build to $LOCAL_PEEL_SYSTEMS/flink-0.9.0"
-    rm -rf $LOCAL_PEEL_SYSTEMS/flink-0.9.0
-    cp -R flink-0.9.0 $LOCAL_PEEL_SYSTEMS/flink-0.9.0
+    echo ">> Copying flink build to $LOCAL_PEEL_SYSTEMS/$FLINK_FOLDER"
+    rm -rf $LOCAL_PEEL_SYSTEMS/$FLINK_FOLDER
+    cp -R $FLINK_FOLDER $LOCAL_PEEL_SYSTEMS/$FLINK_FOLDER
 fi
 
+# copy build to peel systems on wally
 if [ -z "$WALLY_PEEL_SYSTEMS" ]; then
     echo "!! variable WALLY_PEEL_SYSTEMS has not been set"
     echo "(should be e.g. [user@]wally-master.cit.tu-berlin.de:/home/[user]/experiments/adaptive-iterations/systems)"
 else
-    echo ">> Copying flink build to $WALLY_PEEL_SYSTEMS/flink-0.9.0"
-    scp -r flink-0.9.0 $WALLY_PEEL_SYSTEMS
+    echo ">> Copying flink build to $WALLY_PEEL_SYSTEMS/$FLINK_FOLDER"
+    scp -r $FLINK_FOLDER $WALLY_PEEL_SYSTEMS
 fi
 
-mv flink-0.9.0 build-target
+mv $FLINK_FOLDER build-target
